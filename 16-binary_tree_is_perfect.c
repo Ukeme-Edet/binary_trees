b453f8de29a8e4a413bd989296948c6de926a6fc
Ukeme Edet <ukemeedet2207@gmail.com>
2024-02-29 02:21:21 +0100

    Is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -35,6 +35,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (!tree || !(tree->left == tree->right || (tree->left && tree->right)))
 		return (0);
 	if (tree->left)
-		return (1 && binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right) && binary_tree_balance(tree) == 0);
+		return (1 && binary_tree_is_perfect(tree->left) &&
+				binary_tree_is_perfect(tree->right) &&
+				binary_tree_balance(tree) == 0);
 	return (1);
 }
